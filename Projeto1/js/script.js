1feb0fbbc3c697369c058a7c5c1057660f619051
Raphael Leardini B. Roberto <raphaelleardini@hotmail.com>
2018-08-20 11:02:26 -0300

    2a Aula - cardgroup e tooltip

--- Projeto1/js/script.js
+++ Projeto1/js/script.js
@@ -1,7 +1,9 @@
-// $(function() {
-//     $('[data-toggle="tooltip"]').tooltip();
-// });
-//
-// $(function() {
-//     $('[data-toggle="popover"]').popover();
-// });
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+//Libera o tooltip e o popover
